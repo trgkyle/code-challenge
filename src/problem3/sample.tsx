ece311a4189e4703b55be6fc474a1fa6eb11662b
Kyle Ng <kylenyan1215@gmail.com>
2024-12-31 16:49:30 +0700

    update: problem 3

---
+++ src/problem3/sample.tsx
@@ -0,0 +1,81 @@
+interface WalletBalance {
+  currency: string;
+  amount: number;
+}
+interface FormattedWalletBalance {
+  currency: string;
+  amount: number;
+  formatted: string;
+}
+
+interface Props extends BoxProps {}
+const WalletPage: React.FC<Props> = (props: Props) => {
+  const { children, ...rest } = props;
+  const balances = useWalletBalances();
+  const prices = usePrices();
+
+  // ISSUE 1: Type blockchain should need to be defined expect string
+  const getPriority = (blockchain: any): number => {
+    switch (blockchain) {
+      case "Osmosis":
+        return 100;
+      case "Ethereum":
+        return 50;
+      case "Arbitrum":
+        return 30;
+      case "Zilliqa":
+        return 20;
+      case "Neo":
+        return 20;
+      default:
+        return -99;
+    }
+  };
+
+  // ISSUE 2: lhsPriority is undefined, balancePriority isn't used
+  const sortedBalances = useMemo(() => {
+    return balances
+      .filter((balance: WalletBalance) => {
+        const balancePriority = getPriority(balance.blockchain);
+        if (lhsPriority > -99) {
+          if (balance.amount <= 0) {
+            return true;
+          }
+        }
+        return false;
+      })
+      .sort((lhs: WalletBalance, rhs: WalletBalance) => {
+        const leftPriority = getPriority(lhs.blockchain);
+        const rightPriority = getPriority(rhs.blockchain);
+        if (leftPriority > rightPriority) {
+          return -1;
+        } else if (rightPriority > leftPriority) {
+          return 1;
+        }
+      });
+  }, [balances, prices]);
+
+  const formattedBalances = sortedBalances.map((balance: WalletBalance) => {
+    return {
+      ...balance,
+      formatted: balance.amount.toFixed(),
+    };
+  });
+  // ISSUE 3: formattedBalances isn't used
+  const rows = sortedBalances.map(
+    (balance: FormattedWalletBalance, index: number) => {
+      const usdValue = prices[balance.currency] * balance.amount;
+      return (
+        <WalletRow
+          className={classes.row} // ISSUE 4: classes is undefined
+          key={index} // ISSUE 5: Using array index as key is an anti-pattern
+          amount={balance.amount}
+          usdValue={usdValue}
+          formattedAmount={balance.formatted}
+        />
+      );
+    }
+  );
+
+  return <div {...rest}>{rows}</div>;
+};
